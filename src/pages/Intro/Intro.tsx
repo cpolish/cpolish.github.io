c607956a63673f9ea93f8e5597cb802bb89d606b
cpolish <68736710+cpolish@users.noreply.github.com>
2024-04-01 19:52:39 +1100

    add basic animation to intro page

--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -1,9 +1,25 @@
+import "./Intro.css";
+
+import { motion } from "framer-motion";
 import type { JSX } from "react";
 
 import Page from "../../components/Page/Page";
 
-import "./Intro.css";
 
+const ANIMATE_VARIANTS = {
+  initial: {
+    x: -800,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const PERSONAL_DETAILS = {
   firstName: "Chris",
@@ -15,11 +31,20 @@ const PERSONAL_DETAILS = {
 export default function Intro(): JSX.Element {
   return (
     <Page id="intro">
-      <h1>Hi, I'm {PERSONAL_DETAILS.firstName}!</h1>
-      <h2>
-        I'm a software developer and student interested in exploring languages and 
-        frameworks to develop cool projects!
-      </h2>
+      <motion.div
+        className="text-container"
+        variants={ANIMATE_VARIANTS}
+        initial="initial"
+        animate="animate"
+      >
+        <motion.h1 variants={ANIMATE_VARIANTS}>
+          Hi, I'm {PERSONAL_DETAILS.firstName}!
+        </motion.h1>
+        <motion.h2 variants={ANIMATE_VARIANTS}>
+          I'm a software developer and student interested in exploring languages and 
+          frameworks to develop cool projects!
+        </motion.h2>
+      </motion.div>
     </Page>
   );
 }
